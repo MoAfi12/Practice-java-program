86c4c525cb3a9e81230ed56f881e72aea35e6a14
Mohamed Afi <maxamedcaafi60@gmail.com>
2022-11-09 23:24:03 +0300

    New practice in java program

---
+++ src/Project.java
@@ -0,0 +1,41 @@
+//import java.util.Scanner;
+//
+//public class Project {
+//    public static void main(String [] args){
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("please inter your roll number");
+//        int rollno = input.nextInt();
+//        System.out.println("your rollno is" + rollno);
+//    }
+//}
+
+//import java.util.Scanner;
+//
+//public class Project {
+//    public static void main(String[] args){
+//        Scanner input = new Scanner(System.in);
+//
+//        int a = 10;
+//        int b = 20;
+//        int c = a+b;
+//        System.out.println("result the sum of the numbers " + c );
+//    }
+//}
+
+//public class Project {
+//    public static void main(String[] args){
+//        int a = 9;
+//        int b = 2;
+//        int c = a/b ;
+//        System.out.println("the division numbers " + c );
+//    }
+//}
+
+public class Project {
+    public static void main(String[] args){
+        float a = 9 ;
+        float b = 2 ;
+        float c = a/b ;
+        System.out.println("the division of float " + c);
+    }
+}
